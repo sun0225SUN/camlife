78380180e505ffa8d33d36e51cef2aad64531539
Guoqi Sun <sun412926@gmail.com>
2024-09-08 21:17:31 +0800

    feat: optimize login page logic and style

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,15 +1,46 @@
-import { clsx } from "clsx/lite"
-import { Form } from "./form"
+"use client"
 
-export default async function SignInPage() {
+import { useTranslations } from "next-intl"
+import { useEffect } from "react"
+import { useFormState } from "react-dom"
+import toast from "react-hot-toast"
+import { Button } from "~/components/ui/button"
+import { Input } from "~/components/ui/input"
+import { loginAction } from "~/server/auth/actions"
+
+export default function SignInPage() {
+  const t = useTranslations("LoginPage")
+  const [response, action] = useFormState(loginAction, undefined)
+
+  useEffect(() => {
+    if (response === "CredentialsSignInError") {
+      toast.error("账号或密码错误")
+    }
+  }, [response])
+
   return (
-    <div
-      className={clsx(
-        "fixed bottom-0 left-0 right-0 top-0",
-        "flex flex-col items-center justify-center gap-8",
-      )}
-    >
-      <Form />
+    <div className="flex h-screen items-center bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]">
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
+      <form className="z-50 mx-auto w-80 space-y-5" action={action}>
+        <p className="text-center text-2xl text-black dark:text-white">
+          {t("title")}
+        </p>
+        <Input
+          name="email"
+          type="email"
+          placeholder={t("email")}
+          className="h-12"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder={t("password")}
+          className="h-12"
+        />
+        <Button type="submit" className="h-12 w-full">
+          {t("login")}
+        </Button>
+      </form>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import svgToDataUri from "mini-svg-data-uri"
 import type { Config } from "tailwindcss"
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette"
 
-const config = {
+const config: Config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -74,7 +79,34 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+            )}")`,
+          }),
+        },
+        {
+          values: flattenColorPalette(theme("backgroundColor")),
+          type: "color",
+        },
+      )
+    },
+  ],
+}
 
 export default config
