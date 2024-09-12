90a30f485e261e05ea33ad5b4189ecde374f4e23
Guoqi Sun <sun412926@gmail.com>
2024-09-12 19:43:01 +0800

    chore: unified function writing format

--- src/components/photo-info.tsx
+++ src/components/photo-info.tsx
@@ -4,7 +4,7 @@ interface PhotoInfoProps {
   lensModel?: string | null
 }
 
-export const PhotoInfo = ({ make, model, lensModel }: PhotoInfoProps) => {
+export function PhotoInfo({ make, model, lensModel }: PhotoInfoProps) {
   return (
     <div className="my-10 flex w-full justify-center gap-4 px-4 text-xs font-bold md:text-xl">
       <div>{make}</div>
