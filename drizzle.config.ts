274997adb36ba9b6c42336145e5a16e6be2a2d9f
Guoqi Sun <hi@guoqi.dev>
2025-09-11 18:58:44 +0800

    feat: update dependency package versions and deploy to vercel

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,5 +8,5 @@ export default {
 	dbCredentials: {
 		url: env.DATABASE_URL,
 	},
-	tablesFilter: ["camlife_dev_*"],
+	tablesFilter: ["camlife_*"],
 } satisfies Config;
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -10,7 +10,7 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `camlife_dev_${name}`);
+export const createTable = pgTableCreator((name) => `camlife_${name}`);
 
 export const posts = createTable(
 	"post",
