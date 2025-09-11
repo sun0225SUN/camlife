e1f6c28e4e3725960ee2e80b8a0bbdb4ee70c9ad
Guoqi Sun <hi@guoqi.dev>
2025-09-12 00:18:58 +0800

    ci: set Biome rules and format code

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "drizzle-kit";
+import type { Config } from 'drizzle-kit'
 
-import { env } from "@/env";
+import { env } from '@/env'
 
 export default {
-	schema: "./src/server/db/schema.ts",
-	dialect: "postgresql",
-	dbCredentials: {
-		url: env.DATABASE_URL,
-	},
-	tablesFilter: ["camlife_*"],
-} satisfies Config;
+  schema: './src/server/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ['camlife_*'],
+} satisfies Config
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,8 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
-import { index, pgTableCreator } from "drizzle-orm/pg-core";
+import { sql } from 'drizzle-orm'
+import { index, pgTableCreator } from 'drizzle-orm/pg-core'
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -10,18 +10,18 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `camlife_${name}`);
+export const createTable = pgTableCreator((name) => `camlife_${name}`)
 
 export const posts = createTable(
-	"post",
-	(d) => ({
-		id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-		name: d.varchar({ length: 256 }),
-		createdAt: d
-			.timestamp({ withTimezone: true })
-			.default(sql`CURRENT_TIMESTAMP`)
-			.notNull(),
-		updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-	}),
-	(t) => [index("name_idx").on(t.name)],
-);
+  'post',
+  (d) => ({
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    name: d.varchar({ length: 256 }),
+    createdAt: d
+      .timestamp({ withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  }),
+  (t) => [index('name_idx').on(t.name)],
+)
